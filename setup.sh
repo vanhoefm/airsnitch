96651b6ef8bedbf6fec990d3f1368670f4d00235
EtherExists <xzhou114@ucr.edu>
2025-11-06 08:57:51 -0800

    Add network configurations, git submodule, and AE hostapd configs.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 set -e
 
 cd dependencies
+git submodule add https://github.com/vanhoefm/libwifi/
 ./build.sh
 cd ../setup
 ./pysetup.sh
---
+++ setup/setup_network_and_dhcp.sh
@@ -0,0 +1,35 @@
+# Create 4 simulated interfaces. 
+./setup-hwsim.sh 4
+
+# Stop Network Manager
+systemctl stop NetworkManager
+
+airmon-ng check kill
+
+# Add two bridges.
+ip link add name br1 type bridge
+ip link add name br2 type bridge
+
+# Host 2 hostap instances.
+cd ..
+./hostap.py wlan0 --ap --config ./setup/hostapd-wpa3-personal-AE.conf &
+./hostap.py wlan1 --ap --config ./setup/hostapd-wpa2-personal-AE.conf &
+
+ip link set wlan0 master br1
+ip link set br1 up
+ip addr add 192.168.100.1/24 dev br1
+
+ip link set wlan1 master br2
+ip link set br2 up
+ip addr add 192.168.101.1/24 dev br2
+
+# Activate routing capability.
+sysctl -w net.ipv4.ip_forward=1
+
+# Disable systemd-resolved
+systemctl stop systemd-resolved
+
+cd setup
+cp ./br1.conf /etc/dnsmasq.d
+cp ./br2.conf /etc/dnsmasq.d
+systemctl restart dnsmasq
